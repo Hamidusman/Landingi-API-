398a4a84e0f69bc5fffa4db9c5939c23da508cb9
Hamidusman <abdulhamidusman218@gmail.com>
2024-04-10 23:39:28 +0100

    partial_experience_endpoint

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+class Experience(models.Model):
+    Position = models.CharField(max_length = 50)
+    Organisation = models.CharField(max_length = 100)
+    Description = models.CharField(max_length = 1000)
+    Started = models.DateField(max_length = 20)
+    Ended = models.DateField(default = 'Present', max_length = 20)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from .serializers import ExperienceSerializer
+from rest_framework import status
+from rest_framework import generics
+from .models import Experience
 from rest_framework.response import Response
 # Create your views here.
 
 
 
+class experience(generics.ListCreateAPIView):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
